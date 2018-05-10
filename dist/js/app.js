ff203ad4e554bd7d25ccc10864715b4e3460d7d8
cassandradesro <cdesro10@gmail.com>
2018-05-10 10:16:35 -0400

    hamburger

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,2 +1,12 @@
-"use strict";
+'use strict';
+
+var btn = document.querySelector('.js-btn');
+
+function init() {
+    btn.addEventListener('click', function () {
+        btn.classList.toggle('is-active');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', init);
 //# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
+var btn = document.querySelector('.js-btn');
 
+function init() {
+    btn.addEventListener('click', function () {
+        btn.classList.toggle('is-active');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', init);
